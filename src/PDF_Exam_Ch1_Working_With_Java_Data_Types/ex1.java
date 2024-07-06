4c600d474feabed3d8a947c2e1bcfa4847a56cc5
Allson <100061407+Allson-TA@users.noreply.github.com>
2024-07-06 12:26:29 +0800

    java

---
+++ src/PDF_Exam_Ch1_Working_With_Java_Data_Types/ex1.java
@@ -0,0 +1,21 @@
+package PDF_Exam_Ch1_Working_With_Java_Data_Types;
+
+public class ex1 {
+	
+	/* 
+	18: Comparator<String> c1=(j,k)->0;
+	19: Comparator<String> c2=(String j,String k)->0;
+	20: Comparator<String> c3=(var j,String k)->0;
+	21: Comparator<String> c4=(var j,k)->0;
+	22: Comparator<String> c5=(var j,var k)->0;
+
+	How many of these compile?  其中有多少可以編譯？
+	A. 0
+	B. 1
+	C. 2
+	D. 3
+	E. 4
+	F. 5
+	*/
+
+}
